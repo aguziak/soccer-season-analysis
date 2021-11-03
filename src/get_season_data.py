83729be6ec31a5cc4b65b6e9323cdff1362a6aa3
Alexander Guziak <guziakalex@gmail.com>
2021-11-03 18:48:20 -0500

    Reading rapid API key from file

--- src/get_season_data.py
+++ src/get_season_data.py
@@ -1,5 +1,12 @@
 import requests
 import pandas as pd
+import os
+
+
+def _get_rapid_api_key() -> str:
+    with open(os.path.abspath('../api_keys/rapid_api_key.txt')) as file:
+        api_key = file.read()
+    return api_key
 
 
 def get_season_data(season: int, league: str) -> pd.DataFrame:
@@ -14,6 +21,7 @@ def get_season_data(season: int, league: str) -> pd.DataFrame:
         DataFrame: Historical data for the league in the given season
 
     """
+    api_key = _get_rapid_api_key()
     return pd.DataFrame()
 
 
